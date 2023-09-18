0ed492495820582a859296e81778e51b3a0056d1
oneminutelab <93311011+oneminutelab@users.noreply.github.com>
2023-09-18 18:05:46 +0500

    Update index.js
    up

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require ("cors");
 const app = express ();
 
 app.use (cors());
-app.get("/", (req, res) => res.json ("Hellow from iTechNets ..... updated"));
+app.get("/", (req, res) => res.json ("Hello! from iTechNets ..... updated"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('server started on port ${PORT}'));
